89d0f91e7222353c57bfa65a7a0d7f3ee1e91f84
Axel Lells Hjelmqvist <axelhjq@gmail.com>
2023-09-24 22:44:18 +0200

    Configure the nvim-rooter plugin

---
+++ lua/custom/plugins/rooter.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'notjedi/nvim-rooter.lua',
+    config = function ()
+      require'nvim-rooter'.setup({
+        rooter_patterns = { 'src/', '.git', '.hg', '.svn' },
+      })
+    end,
+    dependencies = {}},
+}
+
