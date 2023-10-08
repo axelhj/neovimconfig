c4670fe8df9574ed21c3e84db41100478f0960d2
Axel Lells Hjelmqvist <axelhjq@gmail.com>
2023-10-08 21:03:38 +0200

    Let <C-h|j|k|l>-navigation wrap around edges

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -127,10 +127,10 @@ vim.keymap.set( 'i', '<S-C-Tab>',
 )
 
 -- Quick split/window navigation
-vim.keymap.set('n', '<C-h>', '<C-w>h')
-vim.keymap.set('n', '<C-j>', '<C-w>j')
-vim.keymap.set('n', '<C-k>', '<C-w>k')
-vim.keymap.set('n', '<C-l>', '<C-w>l')
+--vim.keymap.set('n', '<C-h>', '<C-w>h')
+--vim.keymap.set('n', '<C-j>', '<C-w>j')
+--vim.keymap.set('n', '<C-k>', '<C-w>k')
+--vim.keymap.set('n', '<C-l>', '<C-w>l')
 
 vim.keymap.set('n', '<Leader>gd',function()
   vim.cmd(':Bdelete')
