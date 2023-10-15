78708705713fe54837ab2ea48b6f5d8bdb7e47b2
Axel Lells Hjelmqvist <axelhjq@gmail.com>
2023-10-15 23:11:23 +0200

    Keymap (<C-S-e>) to quickly open init.lua

---
+++ after/plugin/openinit.lua
@@ -0,0 +1,6 @@
+-- On windows - edit Neovim config init
+local lua_init_location = vim.fn.stdpath("config") .. "/init.lua"
+
+vim.keymap.set({ 'n' }, '<C-S-e>', ':e ' .. lua_init_location .. '<Cr>', {
+  silent = true, desc = "Edit init.lua"
+})
