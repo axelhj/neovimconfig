af4f682067b6715dc7c90d95fdf0f8031971e079
Axel Lells Hjelmqvist <axelhjq@gmail.com>
2023-10-16 18:33:15 +0200

    Add bufferline offset, sorting, improved mousemap

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -2,5 +2,31 @@ return {
   'akinsho/bufferline.nvim',
   version = "*",
   dependencies = 'nvim-tree/nvim-web-devicons',
-  config = true
+  config = true,
+  opts =  {
+    options = {
+      middle_mouse_command = "bdelete %d",
+      right_mouse_command = "bdelete %d",
+      custom_filter = function(buf_number, buf_numbers)
+        -- filter out filetypes you don't want to see
+        return vim.bo[buf_number].filetype ~= "quickfix" and
+          vim.fn.bufname(buf_number) ~= ""
+      end,
+      offsets = {
+        {
+          filetype = "neo-tree",
+          text = "b:",
+          text_align = "right",
+          separator = true,
+        },
+      },
+      separator_style = "slant",
+      sort_by = function(buffer_a, buffer_b)
+        if (buffer_a.extension == buffer_b.extension) then
+          return buffer_a.id < buffer_b.id
+        end
+        return buffer_a.extension < buffer_b.extension
+      end,
+    },
+  },
 }
