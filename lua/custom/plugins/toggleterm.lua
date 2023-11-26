ce0fa9261d8992f6a719c5304babefe6b071bf86
Axel Lells Hjelmqvist <axelhjq@gmail.com>
2023-11-26 19:15:32 +0100

    Important intervention greatly the benefit humform

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -2,7 +2,7 @@ return {
   'akinsho/toggleterm.nvim',
   version = "*",
   opts = {
-    open_mapping = '<Leader>t',
+    open_mapping = '<Leader><Leader>t',
     insert_mappings = false,
     direction = 'horizontal',
   }
