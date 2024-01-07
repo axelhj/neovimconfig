9cc1ea80a650f9cb544456af90258804a69dfa80
Axel Lells Hjelmqvist <axelhjq@gmail.com>
2024-01-07 18:34:52 +0100

    Implement mksession + source with autocommands

--- lua/plugins/autosession.lua
+++ lua/plugins/autosession.lua
@@ -1,5 +1,13 @@
 local replace_termcodes = require"feedkeys".replace_termcodes
 
+local function split(value, separator)
+  local result = {}
+  value:gsub(
+    "([^"..separator.."]*)"..separator,
+    function(part) table.insert(result, part) end
+  )
+end
+
 local function neotree_toggle()
   if vim.g.NEOTREE_LAST_OPENED then
     require("neo-tree.command")
@@ -13,46 +21,65 @@ local function neotree_toggle()
   end
 end
 
-return {
-  'rmagatti/auto-session',
-  name = 'auto-session',
-  lazy = true,
-  -- Needs to be lazy in order for shada-file to be read before setup.
-  event = "UIEnter",
-  config = function()
-    require'auto-session'.setup {
-      log_level = 'error',
-      auto_session_enable_last_session = true,
-      auto_session_root_dir = vim.fn.stdpath('data').."/sessions/",
-      -- auto_session_enabled is equivalent to auto_save_enabled, auto_restore_enabled
-      auto_session_enabled = true,
-      auto_session_suppress_dirs = {
-        "$USERPROFILE",
-        "$USERPROFILE\\Desktop",
-      },
-      auto_session_use_git_branch = nil,
-      -- The configs below are lua only.
-      bypass_session_save_file_types = nil,
-      pre_save_cmds = {
-        function()
-          -- Store as a global variable such that neotree will open on
-          -- restore if it was open at end of last session.
-          vim.api.nvim_set_var("NEOTREE_LAST_OPENED", require('neotreeopened').is_neotree_open())
-          -- The global variable is not saved on Neovim exit, or saved
-          -- before the autosave is triggered. Write shada explicitly.
-          vim.cmd(":wshada")
-          if vim.g.NEOTREE_LAST_OPENED then
-            vim.cmd(':Neotree close')
-            require"toggleterm".toggle_all("close")
-          end
-        end
-      },
-      post_save_cmds = { neotree_toggle },
-      cwd_change_handling = { restore_upcoming_session = false },
+local function save(file_path)
+  vim.cmd("mksession! " .. file_path)
+end
+
+local function restore(file_path)
+  local cmd = "silent source " .. file_path
+  local success, result = pcall(vim.cmd, cmd)
+  if not success then
+    print("Saving session "..file_path.." failed. "..vim.inspect(result))
+  else
+    print("Did save session "..file_path)
+  end
+end
+
+local function pre_save()
+  -- Store as a global variable such that neotree will open on
+  -- restore if it was open at end of last session.
+  vim.api.nvim_set_var(
+    "NEOTREE_LAST_OPENED",
+    require('neotreeopened').is_neotree_open()
+  )
+  -- The global variable is not saved on Neovim exit, or saved
+  -- before the autosave is triggered. Write shada explicitly.
+  --        vim.cmd(":wshada")
+  if vim.g.NEOTREE_LAST_OPENED then
+    vim.cmd(':Neotree close')
+    require"toggleterm".toggle_all("close")
+  end
+end
+
+local function post_restore()
+  neotree_toggle()
+end
+
+local function init()
+  local session_file_path = vim.fn.expand(vim.fn.stdpath("state") .. "/sessions/session.vim")
+--  options = split(vim.o.sessionoptions, ","),
+  vim.api.nvim_create_autocmd(
+    'VimLeave',
+    {
+      callback = function() 
+        pre_save()
+        save(session_file_path)
+      end
     }
-    if vim.call("argc") == 0 then
-      vim.cmd [[ SessionRestore ]]
-      neotree_toggle()
-    end
-  end,
-}
+  )
+  if vim.call("argc") == 0 then
+    vim.api.nvim_create_autocmd(
+      'UIEnter',
+      {
+        callback = function()
+          restore(session_file_path)
+          post_restore()
+        end
+      }
+    )
+  end
+end
+
+init()
+
+return {}
