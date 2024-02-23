d866ffdbbec1265a5f83467a40d1f8697a5729cb
Axel Lells Hjelmqvist <axelhjq@gmail.com>
2024-02-23 18:54:04 +0100

    Add toggleterm manager (telescope) plugin

---
+++ lua/plugins/toggletermmanager.lua
@@ -0,0 +1,50 @@
+return {
+  "ryanmsnyder/toggleterm-manager.nvim",
+  dependencies = {
+    "akinsho/toggleterm.nvim",
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim", -- only needed because it's a dependency of telescope
+  },
+  opts = {
+    mappings = {
+      i = {
+        ["<Cr>"] = {
+          action = function(prompt_bufnr)
+            require('toggleterm-manager').actions.toggle_term(prompt_bufnr, false)
+            require"feedkeys".replace_termcodes_async('<C-\\><C-n>i')
+          end,
+        },
+        ["<S-Cr>"] = {
+          action = function(prompt_bufnr)
+            require('toggleterm-manager').actions.toggle_term(prompt_bufnr, true)
+            require"feedkeys".replace_termcodes_async('<C-\\><C-n><Esc>')
+          end,
+        },
+        ["<C-i>"] = {
+          action = function(prompt_bufnr)
+            require('toggleterm-manager').actions.create_term(prompt_bufnr, false)
+            require"feedkeys".replace_termcodes_async('<C-\\><C-n>')
+          end,
+        },
+        ["<C-d>"] = {
+          action = function(prompt_bufnr)
+            require('toggleterm-manager').actions.delete_term(prompt_bufnr, false)
+          end,
+        },
+        ["<C-r>"] = {
+          action = function(prompt_bufnr)
+            require('toggleterm-manager').actions.rename_term(prompt_bufnr, false)
+          end,
+        },
+      },
+    },
+  },
+  keys = {
+    {
+      '<Leader>ft',
+      function() require('toggleterm-manager').open({}) end,
+      mode = 'n',
+      desc = 'Toggle the toggleterm-manager [<Leader>ft] (telescope).',
+    },
+  },
+}
