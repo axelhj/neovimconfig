5c62f07161cb767ebb84a6c3bbe3b5c13d9236e4
Axel Lells Hjelmqvist <axelhjq@gmail.com>
2024-04-05 22:40:24 +0200

    Try and improve performance by disabled TS

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,6 +5,15 @@ return {
   },
   build = ":TSUpdate",
   event = { "VeryLazy", },
+  lazy = true,
+  init = function(plugin)
+    -- LazyVim has the following fix from Folke:
+    -- https://github.com/LazyVim/LazyVim/commit/1e1b68d633d4bd4faa912ba5f49ab6b8601dc0c9
+    -- it means that Neotest can propely work with nvim-treesitter when lazy-loaded. Apparently
+    -- Neotest never actually require the nvim-treesitter module.
+    require("lazy.core.loader").add_to_rtp(plugin)
+    require("nvim-treesitter.query_predicates")
+  end,
   config = function()
     require("nvim-treesitter.configs").setup {
       ensure_installed = {
@@ -28,8 +37,8 @@ return {
         enable_autocmd = true,
       },
       auto_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
+      highlight = { enable = false },
+      indent = { enable = false },
     }
   end
 }
