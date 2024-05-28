005acd2d4ada1bf42d2fe7db24013023a1d77331
Axel Lells Hjelmqvist <axelhjq@gmail.com>
2024-05-28 22:21:27 +0200

    Add autcomplete/cmp abort mapping <C-e>

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -36,6 +36,8 @@ return {
       mapping = cmp.mapping.preset.insert {
         ["<C-n>"] = cmp.mapping.select_next_item(),
         ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["Esc"] = cmp.mapping.abort(),
         ["<C-u>"] = cmp.mapping.scroll_docs(-4),
         ["<C-d>"] = cmp.mapping.scroll_docs(4),
         ["<C-b>"] = cmp.mapping.scroll_docs(-8),
