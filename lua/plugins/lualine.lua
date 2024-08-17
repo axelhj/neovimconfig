813202d0e0d3c4dff346522ea8c0372b604cfa49
Axel Lells Hjelmqvist <axelhjq@gmail.com>
2024-08-17 11:00:44 +0200

    Make lualine filename more prominent and stable

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,5 +7,13 @@ return {
       component_separators = "|",
       section_separators = "",
     },
+    sections = {
+      lualine_a = {'filename'},
+      lualine_b = {'mode'},
+      lualine_c = {},
+      lualine_x = {'branch', 'diff', 'diagnostics'},
+      lualine_y = {'encoding', 'fileformat', 'filetype'},
+      lualine_z = {'location'}
+    },
   },
 }
