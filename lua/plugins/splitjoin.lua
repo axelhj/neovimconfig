cece9d3a6812d64604bb5010b5fdb939637f1754
Axel Lells Hjelmqvist <axelhjq@gmail.com>
2025-12-12 15:21:01 +0100

    Switch to non-treesitter splitjoin

--- lua/plugins/splitjoin.lua
+++ lua/plugins/splitjoin.lua
@@ -1,12 +1,29 @@
 return {
-  "Wansmer/treesj",
+  "AndrewRadev/splitjoin.vim",
   event = { "VeryLazy" },
   keys = {
-    { "gs", function() require"treesj".split() end },
-    { "gj", function() require"treesj".join() end },
+    { "gs", ":SplitjoinSplit<cr>", mode = "n" },
+    { "gj", ":SplitjoinJoin<cr>", mode = "n" },
   },
-  opts = {
-    use_default_keymaps = false,
-    max_join_length = 350,
-  },
+  config = function()
+    vim.cmd[[
+      let g:splitjoin_split_mapping = ''
+      let g:splitjoin_join_mapping = ''
+      let g:splitjoin_quiet = 1
+      let g:splitjoin_mapping_fallback = 0
+    ]]
+  end,
 }
+
+-- return {
+--   "Wansmer/treesj",
+--   event = { "VeryLazy" },
+--   keys = {
+--     { "gs", function() require"treesj".split() end },
+--     { "gj", function() require"treesj".join() end },
+--   },
+--   opts = {
+--     use_default_keymaps = false,
+--     max_join_length = 350,
+--   },
+-- }
